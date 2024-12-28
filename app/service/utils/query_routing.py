8cbcfd26a9a2a44f56c2d598a88903803fda12f9
machvikiet <machkiet252003@gmail.com>
2024-12-28 16:59:43 +0700

    update code

--- app/service/utils/query_routing.py
+++ app/service/utils/query_routing.py
@@ -1,17 +1,16 @@
 import py_vncorenlp
-from jpype import isJVMStarted
+#from jpype import isJVMStarted
 import os
 from dotenv import load_dotenv
 
 load_dotenv('.env')
 #os.environ['JAVA_HOME'] = r'C:\Program Files\Java\jdk-23'
 
-if not isJVMStarted():
-    path = "./utils/vncorenlp"
-    path = os.path.abspath(path)
-    path = path.replace('\\', '/')
+path = "./utils/vncorenlp"
+path = os.path.abspath(path)
+path = path.replace('\\', '/')
     #path = r'{}'.format(path)
-    rdrsegmenter = py_vncorenlp.VnCoreNLP(annotators=["wseg"], save_dir=path)
+rdrsegmenter = py_vncorenlp.VnCoreNLP(annotators=["wseg"], save_dir=path)
 
 def segment_vietnamese(text):
     result = rdrsegmenter.word_segment(text)
