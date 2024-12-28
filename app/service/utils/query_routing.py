d6928dc27875bcd7bca28ce4821ef053266ae105
KhoaNguyen <khoanguyenduydang1980@gmail.com>
2024-12-29 00:05:22 +0700

    memory opti

--- app/service/utils/query_routing.py
+++ app/service/utils/query_routing.py
@@ -7,23 +7,16 @@ load_dotenv('.env')
 #os.environ['JAVA_HOME'] = r'C:\Program Files\Java\jdk-23'
 
 # #if not isJVMStarted():
-# path = "./utils/vncorenlp"
-# path = os.path.abspath(path)
-# path = path.replace('\\', '/')
-# #path = r'{}'.format(path)
-# rdrsegmenter = py_vncorenlp.VnCoreNLP(annotators=["wseg"], save_dir=path)
-vncorenlp_path = os.path.abspath('./utils/vncorenlp')
+path = "./utils/vncorenlp"
+path = os.path.abspath(path)
+path = path.replace('\\', '/')
+#path = r'{}'.format(path)
+rdrsegmenter = py_vncorenlp.VnCoreNLP(annotators=["wseg"], save_dir=path)
+#vncorenlp_path = os.path.abspath('./utils/vncorenlp')
 
 def segment_vietnamese(text):
-    # path = "./utils/vncorenlp"
-    # path = os.path.abspath(path)
-    # path = path.replace('\\', '/')
-    #path = r'{}'.format(path)
-    rdrsegmenter = py_vncorenlp.VnCoreNLP(annotators=["wseg"], save_dir=vncorenlp_path)
     result = rdrsegmenter.word_segment(text)
     result = ' '.join([''.join(x) for x in result])
-    del rdrsegmenter
-    os.chdir('../..')
     return result
 
 print("Done.")
