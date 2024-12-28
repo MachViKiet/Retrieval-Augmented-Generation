7383d626423b94c42a33b7712096351c8e3353e1
machvikiet <machkiet252003@gmail.com>
2024-12-28 23:42:26 +0700

    memory opti

--- app/service/utils/query_routing.py
+++ app/service/utils/query_routing.py
@@ -6,15 +6,6 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 #os.environ['JAVA_HOME'] = r'C:\Program Files\Java\jdk-23'
 
-<<<<<<< HEAD
-path = "./utils/vncorenlp"
-path = os.path.abspath(path)
-path = path.replace('\\', '/')
-    #path = r'{}'.format(path)
-rdrsegmenter = py_vncorenlp.VnCoreNLP(annotators=["wseg"], save_dir=path)
-
-def segment_vietnamese(text):
-=======
 # #if not isJVMStarted():
 # path = "./utils/vncorenlp"
 # path = os.path.abspath(path)
@@ -28,7 +19,6 @@ def segment_vietnamese(text):
     path = path.replace('\\', '/')
     #path = r'{}'.format(path)
     rdrsegmenter = py_vncorenlp.VnCoreNLP(annotators=["wseg"], save_dir=path)
->>>>>>> c7036b93522232d5fc6e306506d11b79f09217a1
     result = rdrsegmenter.word_segment(text)
     result = ' '.join([''.join(x) for x in result])
     del rdrsegmenter
