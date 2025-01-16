f9a86836f0deae2da9528fedd17eb8f0f0c37ce2
KhoaNguyen <khoanguyenduydang1980@gmail.com>
2025-01-16 15:27:58 +0700

    temp fix openai

--- app/service/models/model.py
+++ app/service/models/model.py
@@ -71,10 +71,10 @@ class ChatModel:
             if not streaming:
                 text = response.choices[0].message.content
                 return text
-            else:
-                for chunk in response:
-                    if chunk.choices[0].delta.content is not None:
-                        yield chunk.choices[0].delta.content
+            # else:
+            #     for chunk in response:
+            #         if chunk.choices[0].delta.content is not None:
+            #             yield chunk.choices[0].delta.content
         elif self.provider == "Google":
             import google.generativeai as genai
             params = genai.GenerationConfig(max_output_tokens=max_new_tokens)
